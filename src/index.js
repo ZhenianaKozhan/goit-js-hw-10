dd14bbc78c8c153f2247525356ab97c092bdda84
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-02-07 23:22:05 +0200

    done hw

--- src/index.js
+++ src/index.js
@@ -32,29 +32,29 @@ function onInput(evt) {
             })
             .then(data => {
                 if (data.length === 1) {
-                    creatMarcupInfo(data[0])
+                    refs.divInfo.innerHTML = creatMarcupInfo(data[0])
                     refs.list.innerHTML = ""
-                } else if (data.length > 1 && data.length < 10) {
-                    createMarcupList(data); 
+                } else if (data.length > 1 && data.length <= 10) {
+                    refs.list.innerHTML = createMarcupList(data);
                     refs.divInfo.innerHTML =""
                 } 
             })
             .catch(onError)
     }
 }
-  
-function createMarcupList(data) {
-    return refs.list.innerHTML = `
-    <li class="item"><img src="${data[0].flags.svg}" alt="flag ${data[0].name.official}" width="30">  ${data[0].name.official}</li>
-    `
-}
+
+function createMarcupList(countries) {
+    return countries.map(({name, flags}) => `
+    <li class="item"><img src="${flags.svg}" alt="flag ${name.official}" width="30"><b class="name">${name.official}</b></li>`).join("");
+} 
+
 
 function creatMarcupInfo({ flags, name, capital, population, languages }) {
-    return refs.divInfo.innerHTML = `
-    <span><img src="${flags.svg}" alt="flag ${name.official}" width="20">   ${name.official}</span>
-    <p class="key">Capital: <span class="value">${capital}</span></p>
-    <p class="key">Population: <span class="value">${population}</span></p>
-    <p class="key">Languages: <span class="value">${Object.values(languages).join(', ')}</span></p>
+    return  `
+    <span class = "country"><img src="${flags.svg}" alt="flag ${name.official}" width="20">   ${name.official}</span>
+    <p class="key"><b class="name">Capital:</b><span class="value">${capital}</span></p>
+    <p class="key"><b class="name">Population:</b><span class="value">${population}</span></p>
+    <p class="key"><b class="name">Languages:</b><span class="value">${Object.values(languages).join(', ')}</span></p>
 
     `
 }
