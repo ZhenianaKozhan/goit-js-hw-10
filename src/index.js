4604b3a5ade3cc84b355945d1365426818b3a658
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-02-07 09:13:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,26 +17,27 @@ refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(evt) {
     evt.preventDefault();
     const name = evt.target.value.trim();
+
+    refs.list.innerHTML = ""
+    refs.divInfo.innerHTML = ""
+
     if (name.length != 0) {
         fetchCountries(name)
             .then(data => {
-                if (data.length > 4) {
+                if (data.length > 10) {
                     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
                     refs.list.innerHTML = ""
                     refs.divInfo.innerHTML = ""
-                    if (data) {
-                        
-                    }
                 } return data;
             })
             .then(data => {
                 if (data.length === 1) {
                     creatMarcupInfo(data[0])
                     refs.list.innerHTML = ""
-                } else if (data.length > 2 && data.length < 10) {
-                    createMarcupList(data)
+                } else if (data.length > 1 && data.length < 10) {
+                    createMarcupList(data); 
                     refs.divInfo.innerHTML =""
-                }
+                } 
             })
             .catch(onError)
     }
@@ -44,12 +45,9 @@ function onInput(evt) {
   
 function createMarcupList(data) {
     return refs.list.innerHTML = `
-    <li class="item"><img src="${data[0].flags.svg}" alt="flag ${data[0].name.official}" width="20">  ${data[0].name.official}</li>
-    <li class="item"><img src="${data[1].flags.svg}" alt="flag ${data[1].name.official}" width="20">  ${data[1].name.official}</li>
-    <li class="item"><img src="${data[2].flags.svg}" alt="flag ${data[2].name.official}" width="20">  ${data[2].name.official}</li>
-    <li class="item"><img src="${data[3].flags.svg}" alt="flag ${data[3].name.official}" width="20">  ${data[3].name.official}</li>
+    <li class="item"><img src="${data[0].flags.svg}" alt="flag ${data[0].name.official}" width="30">  ${data[0].name.official}</li>
     `
-};
+}
 
 function creatMarcupInfo({ flags, name, capital, population, languages }) {
     return refs.divInfo.innerHTML = `
